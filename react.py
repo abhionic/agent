2770dc522fdd604efbf8d3f9a37843146ae5d53a
Abhishek <48456430+abhionic@users.noreply.github.com>
2026-06-02 20:04:48 -0400

    Update react.py

--- react.py
+++ react.py
@@ -28,7 +28,7 @@ if 'messages' not in st.session_state: st.session_state.messages = []
 def stream(outext):
   def stream_data():
     for word in outext.split(' '): yield word + ' '; time.sleep(0.02)
-  with st.chat_message('assistant'): st.write_stream(stream_data)
+  with st.chat_message('assistant'): return st.write_stream(stream_data)
 
 # display chat messages from history on app rerun
 for message in st.session_state.messages:
